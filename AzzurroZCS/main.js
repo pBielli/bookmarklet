70794aa7bf1e789b5d7b967643ce67a6db94739e
Patrizio Bielli <patrizio.bielli@cemambiente.it>
2024-08-22 10:58:51 +0200

    .

--- AzzurroZCS/main.js
+++ AzzurroZCS/main.js
@@ -1,4 +1,13 @@
+function includeScript(src){
+    const script = document.createElement('script');
+    script.src = src;
+        document.head.appendChild(script);
+    }
+    
+// Includi la libreria XLSX tramite CDN
+includeScript("https://cdn.jsdelivr.net/gh/pBielli/bookmarklet/AzzurroZCS/EnergyDataProcessor.js");
+
 clear();
 // Esecuzione del codice
-const energyDataProcessor = new EnergyDataProcessor();
+var energyDataProcessor = new EnergyDataProcessor();
 console.log(energyDataProcessor.processEnergyData());
