37b7f2a2780240a389a25eefdf28de19e5e73278
Patrizio Bielli <patrizio.bielli@cemambiente.it>
2024-08-27 18:00:23 +0200

    .

--- AzzurroZCS/main.js
+++ AzzurroZCS/main.js
@@ -12,7 +12,8 @@ function startup(){
     includeResource("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js","script");
     includeResource("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css","css");
 
-    setTimeout(()=>{initNavbar();},1000);
+    setTimeout(()=>{initNavbar();},2000);
+    alert("Caricamento in corso...");
 }
 // Funzione per estrarre i dati dalla pagna e salvarli in un file excel
 async function downloadData() {
