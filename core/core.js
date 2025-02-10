87e56e8884c68cdeba927795b2c4fc6021f63bf9
pbielli <patrizio.bielli@cemambiente.it>
2025-02-10 15:34:57 +0100

    primo bookmarklet Wildix

---
+++ core/core.js
@@ -0,0 +1,17 @@
+function fetchGitHubFile(url) {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log('File content:', data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
+  
+  fetchGitHubFile('https://raw.githubusercontent.com/pBielli/bookmarklet/main/core/logo.txt');
