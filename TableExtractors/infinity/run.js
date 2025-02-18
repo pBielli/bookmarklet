e7859ea57b18c865942d5f8d2a12018622f87f28
pbielli <patrizio.bielli@cemambiente.it>
2025-02-18 15:25:50 +0100

    new

--- TableExtractors/infinity/run.js
+++ TableExtractors/infinity/run.js
@@ -1,5 +1,4 @@
-function loadScript(src) {
-    const fullSrc = generateVersionedUrl(src);  // Chiamata alla funzione per aggiungere la versione
+function loadScript(fullSrc) {
     return new Promise((resolve, reject) => {
         const script = document.createElement('script');
         script.src = fullSrc;
@@ -12,16 +11,19 @@ function loadScript(src) {
 function run() {
     const extractor = new TableDataExtractor();
     extractor.extractAndDisplay();
-    var content = extractor.toCSV()
+    var content = extractor.toCSV();
 
-    downloadFile(content, "data.csv", fileType = "text/csv")
-    alert("estrazione completata")
+    downloadFile(content, "data.csv", "text/csv");
+    alert("estrazione completata");
 }
-// loadScript=(url)=>{ document.head.appendChild(Object.assign(document.createElement('script'), { src: url, type: 'text/javascript', async: true })); }
-loadScript("https://pbielli.github.io/bookmarklet/TableExtractors/utils/utils.js")
+
+loadScript("https://pbielli.github.io/bookmarklet/Utils/utils.js")
     .then(() =>
         loadScript("https://pbielli.github.io/bookmarklet/TableExtractors/infinity/dataExtractor_infinity.js")
             .then(() => run())
-            .catch(e => alert('Errore nel caricamento:' + e))
-        )
-    .catch(e => alert('Errore nel caricamento:' + e));
+            .catch(e => alert('Errore nel caricamento di "dataExtractor_infinity.js": ' + (e.message || e)))
+    )
+    .catch(e => {
+        alert('Errore nel caricamento di "utils.js": ' + (e.message || e));
+        console.error(e);
+    });
