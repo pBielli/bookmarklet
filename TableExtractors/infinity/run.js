af5dd692a86d115d74ab410ee52e356b7f2210bb
pbielli <patrizio.bielli@cemambiente.it>
2025-02-18 15:29:54 +0100

    new

--- TableExtractors/infinity/run.js
+++ TableExtractors/infinity/run.js
@@ -21,9 +21,12 @@ loadScript("https://pbielli.github.io/bookmarklet/Utils/utils.js")
     .then(() =>
         loadScript("https://pbielli.github.io/bookmarklet/TableExtractors/infinity/dataExtractor_infinity.js")
             .then(() => run())
-            .catch(e => alert('Errore nel caricamento di "dataExtractor_infinity.js": ' + (e.message || e)))
+            .catch(e => {
+                console.error(e);
+                alert('Errore nel caricamento di "dataExtractor_infinity.js": ' + (e.message || e));
+            })
     )
     .catch(e => {
-        alert('Errore nel caricamento di "utils.js": ' + (e.message || e));
         console.error(e);
+        alert('Errore nel caricamento di "utils.js": ' + (e.message || e));
     });
